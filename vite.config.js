d95b9c176f7fcdddbb9b935dfdceb399c631664d
Anatoly22 <120422@bk.ru>
2023-10-17 15:39:56 +0700

    hotfix

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,20 @@ import { resolve } from 'path';
 import handlebars from 'vite-plugin-handlebars';
 
 export default {
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                text: resolve(__dirname, 'text.html'),
+                solutions: resolve(__dirname, 'solutions.html'),
+            },
+        },
+    },
     plugins: [
         handlebars({
-            partialDirectory: resolve(__dirname, 'components'),
-        })
+            partialDirectory: [
+                resolve(__dirname, 'components'),
+            ]
+        }),
     ],
 };
