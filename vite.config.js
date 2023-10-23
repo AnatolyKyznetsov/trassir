95c91f95888903d5f10b4cfff546fb5f75d6822a
Anatoly22 <120422@bk.ru>
2023-10-23 15:25:18 +0700

    123

--- vite.config.js
+++ vite.config.js
@@ -6,18 +6,22 @@ export default {
     build: {
         rollupOptions: {
             input: {
-                main: resolve(__dirname, 'index.html'),
-                text: resolve(__dirname, 'text.html'),
-                solutions: resolve(__dirname, 'solutions.html'),
-                partner: resolve(__dirname, 'partner.html'),
+                main: resolve(__dirname, 'pages/index.html'),
+                text: resolve(__dirname, 'pages/text.html'),
+                solutions: resolve(__dirname, 'pages/solutions.html'),
+                partner: resolve(__dirname, 'pages/partner.html'),
+                clients: resolve(__dirname, 'pages/clients.html'),
             },
+            // output: {
+                // dir: 'dist'
+            // }
         },
     },
     plugins: [
         handlebars({
             partialDirectory: [
                 resolve(__dirname, 'components'),
-            ]
+            ],
         }),
         viteStaticCopy({
             targets: [
